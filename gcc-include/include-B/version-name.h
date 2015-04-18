ddcd9256e4b1c6e6cb76204d7843fcd163141b6d
yaxinsn <yaxinsn@163.com>
2015-04-18 23:50:24 +0800

    add gcc-include ,and test gcc's option -I-,-iqoute

---
+++ gcc-include/include-B/version-name.h
@@ -0,0 +1,3 @@
+
+
+#define VERNAME  "VER.1.1"
---
+++ gcc-include/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "usrname.h"
+#include <version-name.h>
+
+int main(void)
+{
+        printf(" <%s> \n",USRNAME);
+        printf(" ver <%s> \n",VERNAME);
+}
